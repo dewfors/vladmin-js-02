2b1562fc3aa5e98842afd565327d039d2d5aae39
zaur_malakhov <1zaur1@mail.ru>
2020-09-10 12:25:49 +0300

    004 - Урок 4. JavaScript. Асинхронность.Что такое Event Loop. JS SetTimeout 0

---
+++ 04___eventLoop/index.js
@@ -0,0 +1,23 @@
+// console.log('Start')
+// console.log('Start 2')
+//
+//
+// setTimeout(function () {
+//     console.log('Inside timeout, after 2 seconds')
+// }, 2000)
+//
+// function timeout5sec(){
+//     console.log('timeout5sec')
+// }
+// setTimeout(timeout5sec, 5000)
+//
+// console.log('End')
+
+
+console.log('Start');
+
+setTimeout(function () {
+    console.log('Inside settimeout');
+},0)
+
+console.log('End');
